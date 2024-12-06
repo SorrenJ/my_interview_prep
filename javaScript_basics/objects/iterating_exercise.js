0624b3301048cc965b9f1898473134d8252053c4
SorrenJ <soalexjao@gmail.com>
2024-12-05 19:32:34 -0800

    restucture directories

---
+++ javaScript_basics/objects/iterating_exercise.js
@@ -0,0 +1,13 @@
+const steel = {
+    name: "1084",
+    type: "Carbon Steel",
+    composition: {
+      iron: 98.45,
+      carbon: 0.8,
+      manganese: 0.6,
+      phosphorus: 0.08,
+      sulfur: 0.06,
+    },
+    forging_min_temp: 900,
+    forging_max_temp: 1200,
+  };
