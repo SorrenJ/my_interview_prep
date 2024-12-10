0d2a8e842cc75d07ce9db322d6c4aa5d23cca5ad
SorrenJ <soalexjao@gmail.com>
2024-12-09 21:12:14 -0800

    add DOM solution for getAttribute

---
+++ javaScript_basics/DOM/getAttributeValue/script.js
@@ -0,0 +1,9 @@
+function getAttributes() {
+var idVal = document.getElementById("w3r").href;
+
+alert(`href attribute value is ${idVal}`);
+    
+var relVal = document.getElementById("w3r").rel;
+alert(`href attribute value is ${relVal}`);
+
+}
---
+++ javaScript_basics/DOM/paraColors/script.js
@@ -0,0 +1,1 @@
+document.querySelector('p').style.background ='red';
