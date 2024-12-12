60cd251cbe570d1142396934c9c3db6bd36b6d9e
SorrenJ <soalexjao@gmail.com>
2024-12-12 15:01:51 -0800

    add age array solution

---
+++ TypeScript_Practice/3.Array_type/ageArray.ts
@@ -0,0 +1,11 @@
+/* 
+Create an array called ages that holds the numbers 18, 25, 30. 
+Add a new age 40 to the array. 
+Log the updated array.
+
+*/
+const ages: number[] = [18, 25, 30];  // declared as number[], meaning it can only hold number
+
+ages.push(40);
+
+console.log(ages); // [..., 40]
