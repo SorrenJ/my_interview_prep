2e0209f749f73c8fabe90d8d86e562ddcfff19d3
SorrenJ <soalexjao@gmail.com>
2024-12-12 16:25:04 -0800

    add 2D array solution

--- TypeScript_Practice/3.Array_type/2DArray.js
+++ TypeScript_Practice/3.Array_type/2DArray.js
@@ -6,10 +6,24 @@
     [37, 13, 19]
   ]
 
+  Update the value at position 13, 5 and 10 to 33, 55 and 100 and log the entire matrix.
 */
 const grid = [
     [3, 10, 20],
     [1, 5, 17],
     [37, 13, 19]
 ];
+for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+        if (grid[i][j] === 13) {
+            grid[i][j] = 33;
+        }
+        if (grid[i][j] === 5) {
+            grid[i][j] = 55;
+        }
+        if (grid[i][j] === 10) {
+            grid[i][j] = 100;
+        }
+    }
+}
 console.log(grid);
--- TypeScript_Practice/3.Array_type/2DArray.ts
+++ TypeScript_Practice/3.Array_type/2DArray.ts
@@ -5,6 +5,7 @@
     [37, 13, 19]
   ]
 
+  Update the value at position 13, 5 and 10 to 33, 55 and 100 and log the entire matrix.
 */
 
 
@@ -13,5 +14,26 @@ const grid: number [][] =   [
     [1, 5, 17],
     [37, 13, 19]
   ];
+
+
+for (let i = 0 ; i < grid.length; i++){
+    for (let j = 0; j < grid[i].length; j++) {
+if (grid[i][j] === 13) {
+    grid[i][j] = 33;
+}
+
+if (grid[i][j] === 5) {
+    grid[i][j] = 55;
+}
+
+
+if (grid[i][j] === 10) {
+    grid[i][j] = 100;
+}
+
+    }
+
+}
+
 
   console.log(grid);
