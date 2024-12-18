fa5cc9d43b668fb3d25adebe31c22159a9de7593
SorrenJ <soalexjao@gmail.com>
2024-12-18 14:58:39 -0800

    add simple component that passes props

--- react_Practice/basics/helloworld/src/App.js
+++ react_Practice/basics/helloworld/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import HelloWorld from './components/HelloWorld'
+import Greetings from './components/Greetings'
 
 function App() {
   return (
     <div className="App">
       <HelloWorld/>
+      <Greetings name= "Sorren"/>
     </div>
   );
 }
---
+++ react_Practice/basics/helloworld/src/components/Greetings.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const Greetings = (props) =>{
+    
+    return  <h1>Greetings {props.name} </h1>
+  }
+
+
+export default Greetings;
