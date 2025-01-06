7db3b62ada57476fd8df329bdd2c785f6a0106cc
SorrenJ <soalexjao@gmail.com>
2025-01-06 09:49:32 -0800

    update interview prep readme

---
+++ react_Practice/basics/helloworld/src/components/ListComponent.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+const ListComponent = (props) =>{
+    
+    const items = ["apples", "bananas", "grapes", ]
+    return  <h1>Greetings {props.name} </h1>
+  }
+
+
+export default ListComponent;
