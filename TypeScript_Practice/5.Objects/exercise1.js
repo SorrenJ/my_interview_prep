7a4aa325a1757ef1de8c9950ffd2265a61c8d2c5
SorrenJ <soalexjao@gmail.com>
2025-01-08 11:34:22 -0800

    completed objects exercise1

---
+++ TypeScript_Practice/5.Objects/exercise1.js
@@ -0,0 +1,17 @@
+"use strict";
+/*Exercise: Level 1
+Define an object type Person with two properties: name and age.
+
+Using the Person type defined above, create an object called person1 with your own details.
+
+Extend the Person type to include an optional email property. Then create an object that uses this extended type.
+
+Modify the Person type so that the name property is read-only.
+
+*/
+let person1 = {
+    name: "Sorren",
+    age: 26,
+    email: "soalexjao@gmail.com"
+};
+console.log(person1.name); // Output: "Sorren"
---
+++ TypeScript_Practice/5.Objects/exercise1.ts
@@ -0,0 +1,28 @@
+/*Exercise: Level 1
+Define an object type Person with two properties: name and age.
+
+Using the Person type defined above, create an object called person1 with your own details.
+
+Extend the Person type to include an optional email property. Then create an object that uses this extended type.
+
+Modify the Person type so that the name property is read-only.
+
+*/
+
+interface Person {
+    readonly name:string; 
+    age:number; 
+}
+
+interface User extends Person {
+    email?: string;
+}
+
+let person1: User = {
+    name: "Sorren",
+    age: 26,
+    email: "soalexjao@gmail.com"
+
+};
+
+console.log(person1.name); // Output: "Sorren"
